9f5a7564a930791987c33f8ed003783dc728ad4d
vitaliikravtsov <vitalii.kravtsov@yahoo.com>
2021-04-23 15:43:56 +0300

    add js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
+// Check age
+const body = document.querySelector("body");
+const confirmAge = document.querySelector(".confirmAge");
+const allowedBttn = document.querySelector(".allowed");
+const notAllowedBttn = document.querySelector(".notAllowed");
+const notAllowedPage = document.querySelector(".notAllowedPage");
+
+window.onload = (function () {
+  allowedBttn.addEventListener("click", function () {
+    confirmAge.style.display = "none";
+    body.style.overflowY = "visible";
+  });
+
+  notAllowedBttn.addEventListener("click", function () {
+    notAllowedPage.style.display = "flex";
+  });
+
+  body.style.overflowY = "hidden";
+})();
+//
+
+//Burger menu
 const burgerIcon = document.querySelector(".burger-icon");
+const navbar = document.querySelector(".navbar");
+
+burgerIcon.addEventListener("click", function () {
+  navbar.classList.toggle("navbar_active");
+});
+
+navbar.addEventListener("click", function () {
+  navbar.classList.remove("navbar_active");
+});
+//
---
+++ js/products.js
@@ -0,0 +1,79 @@
+const productsJson = `[
+    {
+        "image":  "../img/wines/Cabernet.jpeg",
+        "name": "Cabernet",
+        "price": "$17,000USD",
+        "type": "red"
+
+    },
+    {
+        "image":  "../img/wines/SauvignonBlanc.jpeg",
+        "name": "Sauvignon Blanc",
+        "price": "$12,000USD",
+        "type": "white"
+
+    },
+    {
+        "image":  "../img/wines/Syrah.jpeg",
+        "name": "Syrah",
+        "price": "$13,000USD",
+        "type": "red"
+
+    },
+    {
+        "image":  "../img/wines/PinotGrigio.jpeg",
+        "name": "Pinot Grigio",
+        "price": "$10,000USD",
+        "type": "white"
+
+    },
+    {
+        "image":  "../img/wines/PinotNoir.jpeg",
+        "name": "Pinot Noir",
+        "price": "$12,000USD",
+        "type": "red"
+
+    },
+    {
+        "image":  "../img/wines/Riesling.jpeg",
+        "name": "Riesling",
+        "price": "$16,000USD",
+        "type": "white"
+
+    },
+    {
+        "image":  "../img/wines/Zifandel.jpeg",
+        "name": "Zifandel",
+        "price": "$14,000USD",
+        "type": "red"
+
+    },
+    {
+        "image": "../img/wines/Soave.jpeg",
+        "name": "Soave",
+        "price": "$12,000USD",
+        "type": "white"
+
+    }
+    
+]`;
+
+function renderProducts(products) {
+  const productsContainer = document.querySelectorAll(".product-list");
+  let html = "";
+  for (const product of products) {
+    html += `
+      <div class="product">
+        <img class="product__img" src="${product.image}" alt="" />
+        <p class="product__name">${product.name}</p>
+        <span class="product__price">${product.price}</span>
+        <button class="product__bttn bttns__bttn">Add to cart</button>
+      </div>
+      `;
+  }
+  productsContainer.forEach((element) => {
+    element.innerHTML = html;
+  });
+}
+
+renderProducts(JSON.parse(productsJson));
